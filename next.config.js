dc34aae324863601ae48dcc08b45e2f7a2acc351
Yang Lee <innovation64feng@gmail.com>
2025-04-20 21:31:24 +0800

    添加 basePath 和 assetPrefix 配置

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    basePath: '/llm-research-platform',
+    assetPrefix: '/llm-research-platform/',
     images: {
       unoptimized: true,
     },
